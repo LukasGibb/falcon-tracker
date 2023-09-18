670dbb4183184eac14a5a71d961ba426a8d43f6b
Conjugato <hi@congugato.com>
2023-09-18 15:29:06 +1000

    update: force destroy, env vars, apis, iam, vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,21 @@
 // -- START: Services
 
+data "google_app_engine_default_service_account" "this" {}
+
 module "project-services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 14.3"
-
+  source     = "terraform-google-modules/project-factory/google//modules/project_services"
+  version    = "~> 14.3"
   project_id = var.project_id
-
   activate_apis = [
-    "compute.googleapis.com",
-    "iam.googleapis.com",
-    "cloudfunctions.googleapis.com",
-    "storage.googleapis.com",
-    "pubsub.googleapis.com",
-    "videointelligence.googleapis.com",
     "cloudbuild.googleapis.com",
-    "cloudscheduler.googleapis.com"
+    "cloudfunctions.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "compute.googleapis.com",
+    "eventarc.googleapis.com",
+    "iam.googleapis.com",
+    "pubsub.googleapis.com",
+    "storage.googleapis.com",
+    "videointelligence.googleapis.com",
   ]
 }
 
@@ -23,9 +24,11 @@ resource "random_id" "suffix" {
 }
 
 resource "google_storage_bucket" "function_bucket" {
-  project    = var.project_id
-  name       = format("%s-%s-%s", var.project_id, "functions", random_id.suffix.hex)
-  location   = "US"
+  project       = var.project_id
+  name          = format("%s-%s-%s", var.project_id, "functions", random_id.suffix.hex)
+  location      = "US"
+  force_destroy = true
+
   depends_on = [module.project-services]
 }
 
@@ -38,7 +41,8 @@ data "archive_file" "comparer_zip" {
   type        = "zip"
   source_dir  = "../cloud-functions/comparer-function"
   output_path = "./tmp/comparer-function.zip"
-  depends_on  = [module.project-services]
+
+  depends_on = [module.project-services]
 }
 
 resource "google_storage_bucket_object" "comparer_object" {
@@ -46,14 +50,14 @@ resource "google_storage_bucket_object" "comparer_object" {
   content_type = "application/zip"
   name         = "src-${data.archive_file.comparer_zip.output_md5}.zip"
   bucket       = google_storage_bucket.function_bucket.name
-  depends_on   = [module.project-services]
+
+  depends_on = [module.project-services]
 }
 
 resource "google_cloudfunctions_function" "comparer_function" {
   name                  = "comparer-function"
   runtime               = "python38"
   project               = var.project_id
-  region                = "us-central1"
   description           = "comparer function"
   trigger_http          = true
   available_memory_mb   = 1024
@@ -62,8 +66,10 @@ resource "google_cloudfunctions_function" "comparer_function" {
   entry_point           = "handle"
 
   environment_variables = {
-    BUCKET_NAME = google_storage_bucket.comparer_bucket.name
-    BYPASS      = false
+    GCP_PROJECT      = var.project_id
+    BUCKET_NAME      = google_storage_bucket.comparer_bucket.name
+    ANNOTATION_TOPIC = google_pubsub_topic.annotation_event_topic.name
+    PERCENTAGE_DIFF  = "0.0"
   }
 
   depends_on = [module.project-services]
@@ -73,19 +79,17 @@ resource "google_cloudfunctions_function_iam_member" "comparer_invoker" {
   project        = google_cloudfunctions_function.comparer_function.project
   region         = google_cloudfunctions_function.comparer_function.region
   cloud_function = google_cloudfunctions_function.comparer_function.name
-
-  role   = "roles/cloudfunctions.invoker"
-  member = "allUsers"
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
 }
 
-resource "google_cloud_scheduler_job" "trigger_comparer_function" {
+resource "google_cloud_scheduler_job" "comparer_schedule" {
   name             = "trigger-comparer-function"
   project          = var.project_id
   description      = "triggers the comparer function to run"
-  schedule         = "*/10 * * * *"
+  schedule         = var.schedule
   time_zone        = "Australia/Melbourne"
   attempt_deadline = "320s"
-  region           = "us-central1"
 
   retry_config {
     retry_count = 3
@@ -100,18 +104,49 @@ resource "google_cloud_scheduler_job" "trigger_comparer_function" {
     body = base64encode(format("{\"video_url\":\"%s\"}", var.video_url))
   }
 
-
   depends_on = [module.project-services]
 }
 
 resource "google_storage_bucket" "comparer_bucket" {
-  project    = var.project_id
-  name       = format("%s-%s-%s", var.project_id, "comparer", random_id.suffix.hex)
-  location   = "US"
+  project       = var.project_id
+  name          = format("%s-%s-%s", var.project_id, "comparer", random_id.suffix.hex)
+  location      = "US"
+  force_destroy = true
+
   depends_on = [module.project-services]
 }
 
 // -- END: Comparer Function
+
+
+// -- START: Annotation Event
+
+resource "google_pubsub_topic" "annotation_event_topic" {
+  name = "annotation-event-topic"
+
+  depends_on = [module.project-services]
+}
+
+resource "google_pubsub_subscription" "annotation_event_subscription" {
+  name                 = "annotation-event-subscription"
+  project              = var.project_id
+  topic                = google_pubsub_topic.annotation_event_topic.name
+  ack_deadline_seconds = 60
+
+  push_config {
+    push_endpoint = google_cloudfunctions_function.annotation_function.https_trigger_url
+  }
+
+  depends_on = [module.project-services]
+}
+
+resource "google_pubsub_topic_iam_binding" "annotate" {
+  topic   = google_pubsub_topic.annotation_event_topic.id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_app_engine_default_service_account.this.email}"]
+}
+
+// -- END: Annotation Event
 
 
 // -- START: Annotation Function
@@ -134,15 +169,20 @@ resource "google_storage_bucket_object" "annotation_object" {
 resource "google_cloudfunctions_function" "annotation_function" {
   name                  = "annotation-function"
   runtime               = "python38"
-  project               = var.project_id
-  region                = "us-central1"
   description           = "annotation function"
   trigger_http          = true
-  available_memory_mb   = 128
+  available_memory_mb   = 1024
   source_archive_bucket = google_storage_bucket.function_bucket.name
   source_archive_object = google_storage_bucket_object.annotation_object.name
   entry_point           = "handle"
-  depends_on            = [module.project-services]
+
+  environment_variables = {
+    GCP_PROJECT  = var.project_id
+    BUCKET_NAME  = google_storage_bucket.annotation_bucket.name
+    NOTIFY_TOPIC = google_pubsub_topic.notify_event_topic.name
+  }
+
+  depends_on = [module.project-services]
 }
 
 resource "google_cloudfunctions_function_iam_member" "annotation_invoker" {
@@ -155,156 +195,84 @@ resource "google_cloudfunctions_function_iam_member" "annotation_invoker" {
 }
 
 resource "google_storage_bucket" "annotation_bucket" {
-  project    = var.project_id
-  name       = format("%s-%s-%s", var.project_id, "annotation", random_id.suffix.hex)
-  location   = "US"
+  name          = format("%s-%s-%s", var.project_id, "annotation", random_id.suffix.hex)
+  location      = "US"
+  force_destroy = true
+
   depends_on = [module.project-services]
 }
 
 // -- END: Annotation Function
 
 
-// -- START: Annotation Bucket Event
+// -- START: Notify Event
 
-resource "google_storage_notification" "notification" {
-  bucket         = google_storage_bucket.annotation_bucket.name
-  payload_format = "JSON_API_V1"
-  topic          = google_pubsub_topic.annotation_event_topic.id
-  event_types    = ["OBJECT_FINALIZE"]
+resource "google_pubsub_topic" "notify_event_topic" {
+  name = "notify-event-topic"
 
-  depends_on = [google_pubsub_topic_iam_binding.binding]
-}
-
-data "google_storage_project_service_account" "gcs_account" {
-  project = var.project_id
-}
-
-resource "google_pubsub_topic_iam_binding" "binding" {
-  project = var.project_id
-  topic   = google_pubsub_topic.annotation_event_topic.id
-  role    = "roles/pubsub.publisher"
-  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
-}
-
-// -- END: Annotation Bucket Event
-
-
-// -- START: Annotation Event
-
-resource "google_pubsub_topic" "annotation_event_topic" {
-  name       = "annotation-event-topic"
-  project    = var.project_id
   depends_on = [module.project-services]
 }
 
-resource "google_pubsub_subscription" "annotation_event_subscription" {
-  name                 = "annotation-event-subscription"
-  project              = var.project_id
-  topic                = google_pubsub_topic.annotation_event_topic.name
-  ack_deadline_seconds = 30
+resource "google_pubsub_subscription" "notify_event_subscription" {
+  name                 = "notify-event-subscription"
+  topic                = google_pubsub_topic.notify_event_topic.name
+  ack_deadline_seconds = 60
+
   push_config {
-    push_endpoint = google_cloudfunctions_function.analyser_function.https_trigger_url
+    push_endpoint = google_cloudfunctions_function.notify_function.https_trigger_url
   }
+
   depends_on = [module.project-services]
 }
 
-// -- END: Annotation Event
+resource "google_pubsub_topic_iam_binding" "notify" {
+  topic   = google_pubsub_topic.notify_event_topic.id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_app_engine_default_service_account.this.email}"]
+}
+
+// -- END: Notify Event
 
 
-// -- START: Analyser Function
+// -- START: Notify Function
 
-data "archive_file" "analyser_zip" {
+data "archive_file" "notify_zip" {
   type        = "zip"
-  source_dir  = "../cloud-functions/analyser-function"
-  output_path = "./tmp/analyser-function.zip"
-  depends_on  = [module.project-services]
+  source_dir  = "../cloud-functions/notify-function"
+  output_path = "./tmp/notify-function.zip"
+
+  depends_on = [module.project-services]
 }
 
-resource "google_storage_bucket_object" "analyser_object" {
-  source       = data.archive_file.analyser_zip.output_path
+resource "google_storage_bucket_object" "notify_object" {
+  source       = data.archive_file.notify_zip.output_path
   content_type = "application/zip"
-  name         = "src-${data.archive_file.analyser_zip.output_md5}.zip"
+  name         = "src-${data.archive_file.notify_zip.output_md5}.zip"
   bucket       = google_storage_bucket.function_bucket.name
-  depends_on   = [module.project-services]
+
+  depends_on = [module.project-services]
 }
 
-resource "google_cloudfunctions_function" "analyser_function" {
-  name                  = "analyser-function"
+resource "google_cloudfunctions_function" "notify_function" {
+  name                  = "notify-function"
   runtime               = "python38"
-  project               = var.project_id
-  region                = "us-central1"
-  description           = "analyser function"
+  description           = "notify function"
   trigger_http          = true
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.function_bucket.name
-  source_archive_object = google_storage_bucket_object.analyser_object.name
+  source_archive_object = google_storage_bucket_object.notify_object.name
   entry_point           = "handle"
-  depends_on            = [module.project-services]
+
+  depends_on = [module.project-services]
 }
 
-resource "google_cloudfunctions_function_iam_member" "analyser_invoker" {
-  project        = google_cloudfunctions_function.analyser_function.project
-  region         = google_cloudfunctions_function.analyser_function.region
-  cloud_function = google_cloudfunctions_function.analyser_function.name
+resource "google_cloudfunctions_function_iam_member" "notify_invoker" {
+  project        = google_cloudfunctions_function.notify_function.project
+  region         = google_cloudfunctions_function.notify_function.region
+  cloud_function = google_cloudfunctions_function.notify_function.name
 
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
 }
 
-// -- END: Analyser Function
-
-
-// -- START: Notification Event
-
-resource "google_pubsub_topic" "notification_event_topic" {
-  name       = "notification-event-topic"
-  project    = var.project_id
-  depends_on = [module.project-services]
-}
-
-resource "google_pubsub_subscription" "notification_event_subscription" {
-  name                 = "notification-event-subscription"
-  project              = var.project_id
-  topic                = google_pubsub_topic.notification_event_topic.name
-  ack_deadline_seconds = 30
-  push_config {
-    push_endpoint = google_cloudfunctions_function.notification_function.https_trigger_url
-  }
-  depends_on = [module.project-services]
-}
-
-// -- END: Notification Event
-
-
-// -- START: Notification Function
-
-data "archive_file" "notification_zip" {
-  type        = "zip"
-  source_dir  = "../cloud-functions/notification-function"
-  output_path = "./tmp/notification-function.zip"
-  depends_on  = [module.project-services]
-}
-
-resource "google_storage_bucket_object" "notification_object" {
-  source       = data.archive_file.notification_zip.output_path
-  content_type = "application/zip"
-  name         = "src-${data.archive_file.notification_zip.output_md5}.zip"
-  bucket       = google_storage_bucket.function_bucket.name
-  depends_on   = [module.project-services]
-}
-
-resource "google_cloudfunctions_function" "notification_function" {
-  name                  = "notification-function"
-  runtime               = "python38"
-  project               = var.project_id
-  region                = "us-central1"
-  description           = "notification function"
-  trigger_http          = true
-  available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.function_bucket.name
-  source_archive_object = google_storage_bucket_object.notification_object.name
-  entry_point           = "handle"
-  depends_on            = [module.project-services]
-}
-
-// -- END: Analyser Function
+// -- END: Notify Function
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "video_url" {
   description = "The youtube video url."
   type        = string
 }
+
+variable "schedule" {
+  description = "The cron schedule for the main function trigger."
+  type        = string
+}
